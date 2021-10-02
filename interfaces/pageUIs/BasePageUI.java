da5cb5958243da82aa037436069d7a864f474594
AnhLe1123 <82967714+AnhLe1123@users.noreply.github.com>
2021-10-03 03:10:48 +0700

    Refactor automation framework

--- interfaces/pageUIs/BasePageUI.java
+++
@@ -1,8 +0,0 @@
-package pageUIs;
-
-public class BasePageUI {
-
-	public static final String HOME_PAGE_LOGO = "//img[@alt='nopCommerce demo store']";
-	public static final String WISH_LIST_HEADER = "//a[@class='ico-wishlist']";
-	public static final String DYNAMIC_LINK_FOOTER = "//div[@class='footer']//a[text()='%s']";
-}
